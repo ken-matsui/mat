973a0a6989592cf676d94e8122a3e0762f11fd76
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2022-08-15 21:09:59 +0900

    Improve Location

--- src/ast/Location.java
+++ src/ast/Location.java
@@ -3,4 +3,7 @@ package mat.ast;
 import mat.parser.Token;
 
 public record Location(String sourceName, Token token) {
+    public String toString() {
+        return sourceName + ":" + token.beginLine;
+    }
 }
