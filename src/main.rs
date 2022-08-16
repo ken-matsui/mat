3f8b29092a737fda746f6b4b92e698fa426413ba
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2022-08-17 07:25:34 +0900

    Create the base of matc

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,55 @@
+mod parser;
+
+use clap::{ArgGroup, Parser};
+
+#[derive(Parser, Debug)]
+#[clap(version, about, long_about = None)]
+#[clap(group(
+    ArgGroup::new("dumps")
+        .args(&[
+            "dump-tokens",
+            "dump-ast",
+            "dump-ref",
+            "dump-sema",
+            "dump-mir",
+            "dump-asm",
+            "print-asm"
+        ]),
+))]
+struct Args {
+    /// Source file to compile
+    source: String,
+
+    /// Dump tokens
+    #[clap(long)]
+    dump_tokens: bool,
+
+    /// Dump AST
+    #[clap(long)]
+    dump_ast: bool,
+
+    /// Dump AST with resolved references
+    #[clap(long)]
+    dump_ref: bool,
+
+    /// Dump semantic analyzed AST
+    #[clap(long)]
+    dump_sema: bool,
+
+    /// Dump MIR
+    #[clap(long)]
+    dump_mir: bool,
+
+    /// Dump structured assembly to stdout
+    #[clap(long)]
+    dump_asm: bool,
+
+    /// Print structured assembly to stdout
+    #[clap(long)]
+    print_asm: bool,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+    println!("Hello, world!: {:?}", args);
 }
