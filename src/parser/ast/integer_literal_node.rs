9a69820b2eebf5468db3eaefce4e4686ccd27b04
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2022-08-18 03:59:22 +0900

    Implement a parser for character

--- src/parser/ast/integer_literal_node.rs
+++ src/parser/ast/integer_literal_node.rs
@@ -76,100 +76,94 @@ pub(crate) fn integer() -> impl Parser<char, IntegerLiteralNode, Error = Simple<
     .padded()
 }
 
-#[test]
-fn integer_test() {
-    assert_eq!(integer().parse("0").ok(), Some(IntegerLiteralNode::I32(0)));
-    assert_eq!(
-        integer().parse("2147483647").ok(),
-        Some(IntegerLiteralNode::I32(2147483647))
-    );
-    assert!(integer().parse("2147483648").is_err());
+pub(crate) fn character() -> impl Parser<char, IntegerLiteralNode, Error = Simple<char>> + Clone {
+    filter(|c: &char| c.is_ascii())
+        .delimited_by(just('\''), just('\''))
+        .map(|c| IntegerLiteralNode::I8(c as i8))
+}
 
-    assert_eq!(
-        integer().parse("0i8 ").ok(),
-        Some(IntegerLiteralNode::I8(0))
-    );
-    assert!(integer().parse("0 i8").is_err());
-    assert_eq!(
-        integer().parse("127i8").ok(),
-        Some(IntegerLiteralNode::I8(127))
-    );
-    assert!(integer().parse("128i8").is_err());
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use chumsky::Parser;
 
-    assert_eq!(
-        integer().parse("0i16 ").ok(),
-        Some(IntegerLiteralNode::I16(0))
-    );
-    assert!(integer().parse("0 i16").is_err());
-    assert_eq!(
-        integer().parse("32767i16").ok(),
-        Some(IntegerLiteralNode::I16(32767))
-    );
-    assert!(integer().parse("32768i16").is_err());
+    #[test]
+    fn integer_test() {
+        assert_eq!(integer().parse("0"), Ok(IntegerLiteralNode::I32(0)));
+        assert_eq!(
+            integer().parse("2147483647"),
+            Ok(IntegerLiteralNode::I32(2147483647))
+        );
+        assert!(integer().parse("2147483648").is_err());
 
-    assert_eq!(
-        integer().parse("0i32 ").ok(),
-        Some(IntegerLiteralNode::I32(0))
-    );
-    assert!(integer().parse("0 i32").is_err());
-    assert_eq!(
-        integer().parse("2147483647i32").ok(),
-        Some(IntegerLiteralNode::I32(2147483647))
-    );
-    assert!(integer().parse("2147483648i32").is_err());
+        assert_eq!(integer().parse("0i8 "), Ok(IntegerLiteralNode::I8(0)));
+        assert!(integer().parse("0 i8").is_err());
+        assert_eq!(integer().parse("127i8"), Ok(IntegerLiteralNode::I8(127)));
+        assert!(integer().parse("128i8").is_err());
 
-    assert_eq!(
-        integer().parse("0i64 ").ok(),
-        Some(IntegerLiteralNode::I64(0))
-    );
-    assert!(integer().parse("0 i64").is_err());
-    assert_eq!(
-        integer().parse("9223372036854775807i64").ok(),
-        Some(IntegerLiteralNode::I64(9223372036854775807))
-    );
-    assert!(integer().parse("9223372036854775808").is_err());
+        assert_eq!(integer().parse("0i16 "), Ok(IntegerLiteralNode::I16(0)));
+        assert!(integer().parse("0 i16").is_err());
+        assert_eq!(
+            integer().parse("32767i16"),
+            Ok(IntegerLiteralNode::I16(32767))
+        );
+        assert!(integer().parse("32768i16").is_err());
 
-    assert_eq!(
-        integer().parse("0u8 ").ok(),
-        Some(IntegerLiteralNode::U8(0))
-    );
-    assert!(integer().parse("0 u8").is_err());
-    assert_eq!(
-        integer().parse("255u8").ok(),
-        Some(IntegerLiteralNode::U8(255))
-    );
-    assert!(integer().parse("256u8").is_err());
+        assert_eq!(integer().parse("0i32 "), Ok(IntegerLiteralNode::I32(0)));
+        assert!(integer().parse("0 i32").is_err());
+        assert_eq!(
+            integer().parse("2147483647i32"),
+            Ok(IntegerLiteralNode::I32(2147483647))
+        );
+        assert!(integer().parse("2147483648i32").is_err());
 
-    assert_eq!(
-        integer().parse("0u16 ").ok(),
-        Some(IntegerLiteralNode::U16(0))
-    );
-    assert!(integer().parse("0 u16").is_err());
-    assert_eq!(
-        integer().parse("65535u16").ok(),
-        Some(IntegerLiteralNode::U16(65535))
-    );
-    assert!(integer().parse("65536u16").is_err());
+        assert_eq!(integer().parse("0i64 "), Ok(IntegerLiteralNode::I64(0)));
+        assert!(integer().parse("0 i64").is_err());
+        assert_eq!(
+            integer().parse("9223372036854775807i64"),
+            Ok(IntegerLiteralNode::I64(9223372036854775807))
+        );
+        assert!(integer().parse("9223372036854775808").is_err());
 
-    assert_eq!(
-        integer().parse("0u32 ").ok(),
-        Some(IntegerLiteralNode::U32(0))
-    );
-    assert!(integer().parse("0 u32").is_err());
-    assert_eq!(
-        integer().parse("4294967295u32").ok(),
-        Some(IntegerLiteralNode::U32(4294967295))
-    );
-    assert!(integer().parse("4294967296u32").is_err());
+        assert_eq!(integer().parse("0u8 "), Ok(IntegerLiteralNode::U8(0)));
+        assert!(integer().parse("0 u8").is_err());
+        assert_eq!(integer().parse("255u8"), Ok(IntegerLiteralNode::U8(255)));
+        assert!(integer().parse("256u8").is_err());
 
-    assert_eq!(
-        integer().parse("0u64 ").ok(),
-        Some(IntegerLiteralNode::U64(0))
-    );
-    assert!(integer().parse("0 u64").is_err());
-    assert_eq!(
-        integer().parse("18446744073709551615u64").ok(),
-        Some(IntegerLiteralNode::U64(18446744073709551615))
-    );
-    assert!(integer().parse("18446744073709551616u64").is_err());
+        assert_eq!(integer().parse("0u16 "), Ok(IntegerLiteralNode::U16(0)));
+        assert!(integer().parse("0 u16").is_err());
+        assert_eq!(
+            integer().parse("65535u16"),
+            Ok(IntegerLiteralNode::U16(65535))
+        );
+        assert!(integer().parse("65536u16").is_err());
+
+        assert_eq!(integer().parse("0u32 "), Ok(IntegerLiteralNode::U32(0)));
+        assert!(integer().parse("0 u32").is_err());
+        assert_eq!(
+            integer().parse("4294967295u32"),
+            Ok(IntegerLiteralNode::U32(4294967295))
+        );
+        assert!(integer().parse("4294967296u32").is_err());
+
+        assert_eq!(integer().parse("0u64 "), Ok(IntegerLiteralNode::U64(0)));
+        assert!(integer().parse("0 u64").is_err());
+        assert_eq!(
+            integer().parse("18446744073709551615u64"),
+            Ok(IntegerLiteralNode::U64(18446744073709551615))
+        );
+        assert!(integer().parse("18446744073709551616u64").is_err());
+    }
+
+    #[test]
+    fn character_test() {
+        assert_eq!(character().parse("'a'"), Ok(IntegerLiteralNode::I8(97)));
+        assert_eq!(character().parse("'1'"), Ok(IntegerLiteralNode::I8(49)));
+        assert_eq!(character().parse("'\n'"), Ok(IntegerLiteralNode::I8(10)));
+        assert!(character().parse("'a").is_err());
+        assert!(character().parse("a'").is_err());
+        assert!(character().parse("a").is_err());
+        assert!(character().parse("'aa'").is_err());
+        assert!(character().parse("'\nn'").is_err());
+    }
 }
