b21fdc90e0d08080529db2740111888a7dc95a20
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2022-08-18 09:15:49 +0900

    Rename expr_node to expr

--- src/parser/ast.rs
+++ src/parser/ast.rs
@@ -1,9 +1,9 @@
-mod expr_node;
+mod expr;
 mod integer_literal_node;
 mod string_literal;
 mod variable;
 
-pub(crate) use expr_node::*;
+pub(crate) use expr::*;
 pub(crate) use integer_literal_node::*;
 pub(crate) use string_literal::*;
 pub(crate) use variable::*;
