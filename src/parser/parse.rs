11d5cacf260e6868eaebe35fd38007e4fc688b7e
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2022-08-18 16:43:02 +0900

    Add comment tests

--- src/parser/parse.rs
+++ src/parser/parse.rs
@@ -3,18 +3,51 @@ use chumsky::prelude::*;
 
 pub type Span = std::ops::Range<usize>;
 
-pub(crate) fn parse(src: String) -> Result<AST, Vec<Simple<char>>> {
+fn comment() -> impl Parser<char, (), Error = Simple<char>> + Clone {
     let single_line_comment = just::<_, _, Simple<char>>("//")
-        .then(take_until(text::newline()))
+        .then_ignore(take_until(text::newline()))
         .ignored();
 
     let multi_line_comment = just::<_, _, Simple<char>>("/*")
         .then(take_until(just("*/")))
         .ignored();
 
-    let comments = single_line_comment.or(multi_line_comment);
+    single_line_comment.or(multi_line_comment)
+}
 
+pub(crate) fn parse(src: String) -> Result<AST, Vec<Simple<char>>> {
     compilation_unit()
-        .padded_by(comments.padded().repeated())
+        .padded_by(comment().padded().repeated())
         .parse(src)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use chumsky::Parser;
+
+    #[test]
+    fn comment_test() {
+        assert!(comment().parse("// comment\n").is_ok());
+        assert!(comment().parse("//\n").is_ok());
+        assert!(comment().parse("// comment").is_err());
+        assert!(comment().parse("/\n").is_err());
+
+        assert!(comment()
+            .parse(
+                r#"/*
+                comment
+                */
+            "#
+            )
+            .is_ok());
+        assert!(comment().parse("/**/\n").is_ok());
+        assert!(comment().parse("/* foo */\n").is_ok());
+        assert!(comment().parse("/** *foo **/\n").is_ok());
+        assert!(comment().parse("/* foo */").is_ok());
+        assert!(comment().parse("/* foo *\n").is_err());
+        assert!(comment().parse("/* foo \n").is_err());
+        assert!(comment().parse("* foo */\n").is_err());
+        assert!(comment().parse(" foo */\n").is_err());
+    }
 }
