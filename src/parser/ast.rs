b1c7f92be6224ea6d106516c745c609547e73e34
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2022-08-18 12:40:38 +0900

    Create ty.rs

--- src/parser/ast.rs
+++ src/parser/ast.rs
@@ -2,10 +2,12 @@ mod expr;
 mod integer;
 mod stmt;
 mod string;
+mod ty;
 mod variable;
 
 pub(crate) use expr::*;
 pub(crate) use integer::*;
 pub(crate) use stmt::*;
 pub(crate) use string::*;
+pub(crate) use ty::*;
 pub(crate) use variable::*;
---
+++ src/parser/ast/ty.rs
@@ -0,0 +1,50 @@
+/// Type Node
+use chumsky::prelude::*;
+
+#[derive(Debug, PartialEq, Clone)]
+pub(crate) enum Type {
+    Void,
+    I8,
+    I16,
+    I32,
+    I64,
+    U8,
+    U16,
+    U32,
+    U64,
+    /// User defined type
+    User(String),
+}
+
+pub(crate) fn typeref() -> impl Parser<char, Type, Error = Simple<char>> + Clone {
+    choice((
+        text::keyword("void").to(Type::Void),
+        text::keyword("char").to(Type::I8),
+        text::keyword("i8").to(Type::I8),
+        text::keyword("i16").to(Type::I16),
+        text::keyword("i32").to(Type::I32),
+        text::keyword("i64").to(Type::I64),
+        text::keyword("u8").to(Type::U8),
+        text::keyword("u16").to(Type::U16),
+        text::keyword("u32").to(Type::U32),
+        text::keyword("u64").to(Type::U64),
+        text::ident().map(Type::User),
+    ))
+}
+
+#[derive(Debug, PartialEq, Clone)]
+pub(crate) struct TypeDef {
+    new: String,
+    old: Type,
+}
+
+/// type new = old;
+pub(crate) fn typedef() -> impl Parser<char, TypeDef, Error = Simple<char>> + Clone {
+    text::keyword("type")
+        .padded()
+        .then(text::ident::<_, Simple<char>>())
+        .then_ignore(just('='))
+        .then(typeref())
+        .then_ignore(just(';'))
+        .map(|(((), new), old)| TypeDef { new, old })
+}
